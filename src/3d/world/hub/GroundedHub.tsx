2bfac0d17f1213f8321f832ccf2d104b160ddf8a
Space <ndoemo02@gmail.com>
2026-05-17 07:03:12 +0200

    Split hub and corridor loading boundaries

--- src/3d/world/hub/GroundedHub.tsx
+++ src/3d/world/hub/GroundedHub.tsx
@@ -4,21 +4,32 @@ import { Suspense } from 'react';
 import { LeftWingCorridor } from '../corridors/LeftWingCorridor';
 import { HubShell } from './HubShell';
 
-function HubLoadingFallback() {
+function HubShellFallback() {
   return (
     <group>
       <ambientLight intensity={0.8} />
-      <pointLight position={[-24, 4.5, -3]} intensity={4} distance={14} color="#f5e6d0" />
       <pointLight position={[0, 5, 0]} intensity={2} distance={18} color="#dbeafe" />
 
       <mesh position={[0, -0.06, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
         <planeGeometry args={[30, 20]} />
         <meshStandardMaterial color="#d6cec0" roughness={0.55} metalness={0.04} />
       </mesh>
+    </group>
+  );
+}
+
+function CorridorFallback() {
+  return (
+    <group>
+      <pointLight position={[-24, 4.5, -5]} intensity={4} distance={16} color="#f5e6d0" />
 
       <mesh position={[-25, -0.055, -5]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
         <planeGeometry args={[30, 8]} />
         <meshStandardMaterial color="#c9c2b7" roughness={0.6} metalness={0.03} />
+      </mesh>
+      <mesh position={[-25, 6, -5]} rotation={[Math.PI / 2, 0, 0]} receiveShadow>
+        <planeGeometry args={[30, 8]} />
+        <meshStandardMaterial color="#ddd5ca" roughness={0.74} metalness={0.02} />
       </mesh>
       <mesh position={[-25, 3, -9]} castShadow receiveShadow>
         <boxGeometry args={[30, 6, 0.35]} />
@@ -29,6 +40,10 @@ function HubLoadingFallback() {
         <meshStandardMaterial color="#8a8378" roughness={0.82} />
       </mesh>
       <mesh position={[-10, 3, -5]} castShadow receiveShadow>
+        <boxGeometry args={[0.35, 6, 8]} />
+        <meshStandardMaterial color="#6d6861" roughness={0.84} />
+      </mesh>
+      <mesh position={[-39.8, 3, -5]} castShadow receiveShadow>
         <boxGeometry args={[0.35, 6, 8]} />
         <meshStandardMaterial color="#6d6861" roughness={0.84} />
       </mesh>
@@ -47,8 +62,11 @@ export function GroundedHub({ onEnterRoom }: { onEnterRoom?: (id: string) => voi
       <hemisphereLight args={['#fff8f0', '#c8bfb4', 0.7]} />
       <directionalLight position={[5, 10, -3]} intensity={0.6} color="#fff8f0" castShadow />
 
-      <Suspense fallback={<HubLoadingFallback />}>
+      <Suspense fallback={<HubShellFallback />}>
         <HubShell />
+      </Suspense>
+
+      <Suspense fallback={<CorridorFallback />}>
         {onEnterRoom && <LeftWingCorridor onEnterRoom={onEnterRoom} />}
       </Suspense>
     </group>
